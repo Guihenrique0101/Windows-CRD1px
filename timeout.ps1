00d639d53dc2ea0102b4020d8f91f6fbff2e31ef
Guihenrique0101 <123760028+Guihenrique0101@users.noreply.github.com>
2024-05-12 09:56:58 -0300

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -1,1 +1,2 @@
-timeout 21600
+
+Start-Process -FilePath powershell.exe -ArgumentList '-Command Start-Sleep -Seconds 4' -PassThru | Wait-Process -Timeout $maximumRuntimeSeconds -ErrorAction Stop | Write-Warning -Message 'Process successfully completed within timeout.' 
